65316dca54cbb8681ffa2c722898bc1d9cca396b
Niccolo Antonelli Dziri <niccolo.dz.antonelli@gmail.com>
2023-08-19 15:19:29 +0200

    Ajout d'un bot qui choisit selon une distribution uniforme

--- Puissance4/Bot.cpp
+++ Puissance4/Bot.cpp
@@ -1,15 +1,47 @@
 #include "Bot.h"
 
+using namespace std;
+
 int puissance4::bots::Player::genMove(const grilleDeJeu::Plateau& jeu)
 {
     int col = -1;
-    affichageConsole::all(jeu);
-    DemanderColonne:
-    std::cout << "Entrer num colonne: ";
-    std::cin >> col;
-    if (!jeu.isColValide(col)) {
-        std::cout << "La colonne " << col << " n'est pas valide";
-        goto DemanderColonne;
-    }
+    do
+    {
+        cout << "Entrer num colonne: ";
+        cin >> col;
+        if (!jeu.isColValide(col))
+            cout << "La colonne " << col << " n'est pas valide";
+
+    } while (!jeu.isColValide(col));
+
     return col;
 }
+
+int puissance4::bots::Uniform::genMove(const grilleDeJeu::Plateau& jeu)
+{
+    random_device rd;
+    default_random_engine eng(rd());
+
+    vector<int> tab = creerTabCoupsPossibles(jeu);
+
+    uniform_int_distribution<int> distrib(0, tab.size() - 1);
+
+    return tab[distrib(eng)];
+}
+
+std::vector<int> puissance4::bots::Uniform::creerTabCoupsPossibles(const grilleDeJeu::Plateau& jeu) const
+{
+    std::vector<int> tab;
+    for (int i = 0; i < jeu.getLargeur(); i++)
+    {
+        if (jeu.isColValide(i))
+        {
+            tab.push_back(i);
+        }
+    }
+    return tab;
+}
+
+
+
+
--- Puissance4/Bot.h
+++ Puissance4/Bot.h
@@ -1,33 +1,51 @@
 #pragma once
 
 #include <iostream>
-#include <string> // Pour le nom du Bot
+#include <vector>
+#include <random>
 #include "Plateau.h"
 #include "Affichage.h"
 
 namespace puissance4 {
 	namespace bots {
 
+		// Classe mère de tous les bots
 		class Bot {
 		public:
-			Bot(const std::string nomBot) : nomBot(nomBot){}
+			Bot() = default;
 
 			virtual ~Bot() = default;
 
 			virtual int genMove(const grilleDeJeu::Plateau& jeu) = 0;
-
-		private:
-			std::string nomBot;
 		};
 
-		/*
-		* Pas un bot à proprement parler mais c'est l'utilisateur qui choisit quelle colonne jouer
-		*/
+		// Pas un bot à proprement parler mais c'est l'utilisateur qui choisit quelle colonne jouer
 		class Player : public Bot {
 		public:
-			Player() : Bot("humain"){}
+			Player() = default;
 
+			// Demande au joueur d'entrer le coup qu'il veut jouer
 			int genMove(const grilleDeJeu::Plateau& jeu) override;
+		};
+
+		// Classe mère des bots qui utilisent de l'aléatoire
+		class Random : public Bot {
+		public:
+			Random() = default;
+
+		};
+
+		class Uniform : public Random {
+		public:
+			Uniform() = default;
+
+			//
+			int genMove(const grilleDeJeu::Plateau& jeu) override;
+
+		protected:
+			// Crée un tableau dans tab avec les coups qu'il est possible de jouer
+			std::vector<int> creerTabCoupsPossibles(const grilleDeJeu::Plateau& jeu) const;
+
 		};
 	}
 }
